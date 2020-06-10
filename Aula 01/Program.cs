c94b21b896ab6ddf15a168b49880fc9124559fe9
AlucardHell <alucard_gm@hotmail.com>
2020-06-10 20:25:53 -0300

    Finalizado Editar  Aluno. Modificação no design do Menu.

--- Aula 01/Program.cs
+++ Aula 01/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace Aula_1
 {
     class Program
@@ -28,6 +29,7 @@ namespace Aula_1
                         Console.WriteLine("Digite o Periodo: ");
                         int addPeriodo = int.Parse(Console.ReadLine());
                         alunos.Add(new Aluno(addNome, addMatricula, addCpf, addPeriodo));
+                        Console.WriteLine("Cadastro efetuado com sucesso!!");
                         i = noMenu();
                         if (i == 1)
                         {
@@ -67,6 +69,27 @@ namespace Aula_1
                     case 4:
                         Console.Clear();
                         Console.WriteLine("Vamos Editar Aluno:");
+                        Console.Write("Digite a Matricula para Editar Aluno: ");
+                        int editAluno = int.Parse(Console.ReadLine());
+
+                        foreach (Aluno a in alunos)
+                        {
+                            if (a.matricula == editAluno)
+                            {
+                                Console.WriteLine("==================================");
+                                Console.WriteLine("Digite o Nome: ");
+                                string editNome = Console.ReadLine();
+                                Console.WriteLine("CPF: ");
+                                string editCPF = Console.ReadLine();
+                                Console.WriteLine("Periodo: ");
+                                int editPeriodo = int.Parse(Console.ReadLine());
+                                a.nome = editNome;
+                                a.cpf = editCPF;
+                                a.periodo = editPeriodo;
+                                Console.WriteLine("==================================\n");
+                            }
+                        }
+
                         i = noMenu();
                         if (i == 1)
                         {
@@ -93,14 +116,17 @@ namespace Aula_1
             while (confirm)
             {
                 Console.Clear();
-                Console.WriteLine("SISTEMA ESCOLAR DA TURMA DO PROF° KAIN.");
-                Console.WriteLine("Menu:");
-                Console.WriteLine("1) Cadastrar Aluno:");
-                Console.WriteLine("2) Listar Alunos: ");
-                Console.WriteLine("3) Excluir Alunos:");
-                Console.WriteLine("4) Editar Aluno: ");
-                Console.WriteLine("0) Sair: ");
-                Console.WriteLine("Digite uma opção acima:");
+                Console.WriteLine(" _____________________________________________");
+                Console.WriteLine("|   SISTEMA ESCOLAR DA TURMA DO PROF° KAIN.   |");
+                Console.WriteLine("|                Menu:                        |");
+                Console.WriteLine("|       1) Cadastrar Aluno:                   |");
+                Console.WriteLine("|       2) Listar Alunos:                     |");
+                Console.WriteLine("|       3) Excluir Alunos:                    |");
+                Console.WriteLine("|       4) Editar Aluno:                      |");
+                Console.WriteLine("|       0) Sair:                              |");
+                Console.WriteLine("|_____________________________________________|");
+                Console.WriteLine("|       Digite uma opção acima:               |");
+                Console.WriteLine("|_____________________________________________|");
                 opcao = int.Parse(Console.ReadLine());
 
                 if (opcao >= 0 && opcao < 5)
@@ -122,6 +148,7 @@ namespace Aula_1
             int a = 99;
             while (a == 99)
             {
+                Console.Clear();
                 Console.WriteLine("Deseja voltar ao Menu?");
                 Console.WriteLine("Digite 1 para voltar.");
                 Console.WriteLine("Digite 0 para SAIR!!!");
